735836a8a665aeaf67a538769c784c3a0fa3dacf
Jeremy McWilliams <jeremym@lclark.edu>
2025-06-06 22:35:38 +0000

    created hideous line plot

--- testing.R
+++ testing.R
@@ -15,6 +15,7 @@ library(sf)
 
 waVehicleData<-read_csv("Vehicle_Registrations_by_Class_and_County.csv")
 
+# see what we're dealing with
 unique(waVehicleData$`Fiscal Year`)
 unique(waVehicleData$`Fuel Type`)
 unique(waVehicleData$`Residential County`)
@@ -24,15 +25,16 @@ waVehicleData <-waVehicleData %>%
   filter(`Residential County` !="Out Of State")
 
 
-
+#limit to electric/hybrid
 hybridElectricOnly<-waVehicleData %>%
   filter(`Fuel Type`=="Electric" | `Fuel Type`=="Hybrid")
 
+# aggregate
 byCountyByYear<-hybridElectricOnly %>%
   group_by(`Residential County`, `Fiscal Year`) %>%
   summarize(numPurchased=n())
 
-
+# no acs5 data > 2023, so limit
 byCountyByYear <-byCountyByYear %>%
   filter(`Fiscal Year`<2024)
 
@@ -41,11 +43,12 @@ write_csv(byCountyByYear, "data.csv")
 
 #tidycensus
 
+#probably need to clean this up
 Sys.getenv("CENSUS_API_KEY") |> 
   census_api_key(install = TRUE)
 readRenviron("~/.Renviron")
 
-
+# sample pop data for 2022 from tidycensus
 wa_pop <- get_acs(
   geography = "county",
   state = "WA",
@@ -64,7 +67,7 @@ wa_pop <- get_acs(
 waPopTest <- wa_pop %>%
   mutate(`Residential County` = str_remove(NAME, " County, Washington$"))
 
-
+#limit orig data to 2022
 wa2022<-byCountyByYear %>%
   filter(`Fiscal Year`==2022)
 
@@ -81,6 +84,48 @@ ggplot(merged22) +
   geom_sf(aes(fill = purchasesPerK)) +
   scale_fill_viridis_c() +
   theme_void()
+
+
+#next steps:
+# loop for 2017-2023
+# line plots, showing rate over time by county (not all)
+
+years <- 2017:2023
+
+wa_pop_all_years <- map_dfr(years, function(yr) {
+  get_acs(
+    geography = "county",
+    state = "WA",
+    variables = "B01003_001",
+    year = yr,
+    survey = "acs5",
+    geometry = TRUE
+  ) %>%
+    mutate("Fiscal Year" = yr)
+})
+
+waPopAllYears <- wa_pop_all_years %>%
+  mutate(`Residential County` = str_remove(NAME, " County, Washington$"))
+
+
+# must now join on 2 columns, county & year
+mergedAllYears<-left_join(waPopAllYears, byCountyByYear, by=c("Residential County", "Fiscal Year"))
+
+#calculate rate
+mergedAllYears<-mergedAllYears %>%
+  mutate(purchasesPerK=(numPurchased/estimate)*1000)
+
+
+#messy line plot:
+
+ggplot(data=mergedAllYears, mapping=aes(x=`Fiscal Year`, y=purchasesPerK, colour = `Residential County`))+
+  geom_line()
+
+
+
+# maybe look at 1 county, with rates for different types of cars
+# Clark county
+
 
 
 
